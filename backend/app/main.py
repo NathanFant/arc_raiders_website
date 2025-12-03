bfafb56a43cbc37504a1643f7c5aa83ac68e89dc
NathanF <nfant01@gmail.com>
2025-12-03 00:57:37 -0600

    weapons/items 1.0

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.v1 import auth
+from app.api.v1 import auth, weapons
 
 app = FastAPI(
     title="Arc Raiders Loadout API",
@@ -20,6 +20,7 @@ app.add_middleware(
 
 # Incude routers
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
+app.include_router(weapons.router, prefix="/api/v1/weapons", tags=["weapons"])
 
 
 # Health check endpoints
